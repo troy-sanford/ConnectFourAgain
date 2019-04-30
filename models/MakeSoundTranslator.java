118541998daed6439e9549087be10f6c5e0e6b74
Emily Baker <emilybaker@ems-macbook-pro.local>
2019-04-30 13:37:41 -0400

    added classes to connect to external API

---
+++ models/MakeSoundTranslator.java
@@ -0,0 +1,19 @@
+package models;
+
+/*
+    last edited: 04/30/19
+    author: Troy Sanford
+    purpose: Translator class to rereoute data to/from MakeSound
+*/
+
+public class MakeSoundTranslator {
+
+    public static void playClink() {
+        MakeSound.playClink();
+    }
+
+    public static void playVictory() {
+        MakeSound.playVictory();
+    }
+
+}
